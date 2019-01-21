d2b898677ae148e56653a968342196d7a2673df8
stepan <stepan@localhost.localdomain>
2019-01-21 20:54:25 +0100

    deleting dead code

--- src/chakra/concepts.h
+++ src/chakra/concepts.h
@@ -4,10 +4,12 @@
 
 #include <type_traits>
 #include <iterator>
-#include <cstring>
 
 namespace ocn::chakra
 {
+  template <typename T1, typename T2>
+  concept bool ConvertibleTo = std::is_convertible<T1, T2>::value;
+
   template <typename T>
   concept bool Int = std::is_integral<T>::value;
 
@@ -20,18 +22,6 @@ namespace ocn::chakra
   template <typename T>
   concept bool Pointer = std::is_pointer<T>::value;
 
-  template <typename T1, typename T2>
-  concept bool ConvertibleTo = std::is_convertible<T1, T2>::value;
-
-  // concept bool Buffer = requires (T b) {
-  //   { std::begin(b) } -> ConvertibleTo<const char*>;
-  //   { std::size(b) } -> Unsigned;
-  // };
-
-  // template <typename T>
-  // concept bool LucidFunctor = requires { void(&T::operator()); };
-
-
   template <typename T>
   concept bool LucidFunctor = requires { void(&T::operator()); };
 
@@ -40,11 +30,4 @@ namespace ocn::chakra
     { std::begin(s) } -> ConvertibleTo<const char*>;
     { std::size(s) } -> Unsigned;
   };
-
-  // template <typename T>
-  // concept bool Callable = requires {
-  //   function_traits<T>::arity >= 0;
-  //   typename function_traits<T>::args;
-  //   typename function_traits<T>::return_type;
-  // };
 }
