2a3b27d618f22b8a1b1df10113b641463d414da6
stepan <stepan@localhost.localdomain>
2019-01-21 21:14:46 +0100

    cosmetics

--- src/chakra/invoke.h
+++ src/chakra/invoke.h
@@ -15,18 +15,18 @@ namespace ocn::chakra
   };
 
   template <typename F, typename... Args>
-    requires JsrtFunction<F, Args...>
-    JsErrorCode invoke(F&& f, Args&&... args)
-    {
-      return std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
-    }
+  requires JsrtFunction<F, Args...>
+  JsErrorCode invoke(F&& f, Args&&... args)
+  {
+    return std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+  }
 
   template <typename F, typename... Args>
-    requires JsrtFunction<F, Args...>
-    void invoke_checked(F&& f, Args&&... args)
-    {
-      JsErrorCode error_code = invoke(std::forward<F>(f), std::forward<Args>(args)...);
-      if (error_code != JsNoError)
-        throw exception(error_code);
-    }
+  requires JsrtFunction<F, Args...>
+  void invoke_checked(F&& f, Args&&... args)
+  {
+    JsErrorCode error_code = invoke(std::forward<F>(f), std::forward<Args>(args)...);
+    if (error_code != JsNoError)
+      throw exception(error_code);
+  }
 }
